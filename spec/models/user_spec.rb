02b020dde25eccf3d5d65b9c03b0c79a1824e22b
Your Name <your.email@example.com>
2013-10-24 10:40:49 +0700

    Add user microposts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe User do
 
   before do
-    @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   subject { @user }
@@ -15,6 +16,8 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
+  it { should respond_to(:microposts) }
 
 
   it { should be_valid }
@@ -42,16 +45,34 @@ describe User do
       it { should eq found_user.authenticate(@user.password) }
     end
 
-    # describe "with invalid password" do
-    #   let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+    it { should respond_to(:microposts) }
+    it { should respond_to(:feed) }
 
-    #   it { should_not eq user_for_invalid_password }
-    #   specify { expect(user_for_invalid_password).to be_false }
-    # end
+    describe "micropost associations" do
 
-    # describe "remember token" do
-    # before { @user.save }
-    # its(:remember_token) { should_not be_blank }
-    # end
+    before { @user.save }
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it "should have the right microposts in the right order" do
+      expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
+    end
+
+    describe "status" do
+      let(:unfollowed_post) do
+        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+
+      its(:feed) { should include(newer_micropost) }
+      its(:feed) { should include(older_micropost) }
+      its(:feed) { should_not include(unfollowed_post) }
+    end
+  end
+end
+
   end
 end
